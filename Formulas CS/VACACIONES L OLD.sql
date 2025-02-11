f0ad8ee5a9ad8670448174f7c9b323c8f1fcdccd
DynamicsAdmin <DynamicsAdmin@gdynamics.com>
2025-02-11 17:27:40 -0600

    Cambios a formulas y funciones

--- Formulas CS/VACACIONES L OLD.sql
+++ Formulas CS/VACACIONES L OLD.sql
@@ -7,6 +7,13 @@ declare @Promedio real;
 declare @Asignacion1 real;
 declare @MesesCalcular real;
 
+declare @param_1 nvarchar(60);
+declare @param_2 nvarchar(60);
+declare @param_3 int;
+declare @param_4 int;
+declare @param_5 int;
+
+
 
 
 -- ## ASIGNACIONES
@@ -17,8 +24,15 @@ set @CalcInvVac = {Monto_Anio_VacHistoricas,G_VACACIONES,Acumulado};
 set @Dias = {Tabla_AguinaldoVacaciones,V,hoy,s};
 set @Dias = {Funcion,DiasVacaciones,@A}; 
 set @MesesCalcular =  {Funcion,CalcMesCompletoFechaIngreso,@Asignacion1};
-set @Promedio = {Monto_Acumulado_Mes,SC,PMeses,G_VACACIONES,11}
-
+set @Promedio = {Monto_Acumulado_Mes,SC,PMeses,G_VACACIONES,11};
+  
+set @param_1 = 'OTRO';
+set @param_2 = 'G_VACACIONES';
+set @param_3 = 12;
+set @param_4 = 1;
+set @param_5 = 3;
+set @Promedio = {Funcion,Monto_Acumulado_Cofal,[@param_1,@param_2,@param_3,@param_4,@param_5]};  
+set @Promedio = @Promedio / 30;
 
 
 
@@ -26,7 +40,73 @@ set @Promedio = {Monto_Acumulado_Mes,SC,PMeses,G_VACACIONES,11}
 
 if ({funcion,DiasLiquidacion} > 0) then
     /*{Total_Acumulado_Anio,MC,D,G_VACACIONES_L,1,pmeses} / 30 * {Funcion,DiasVacaciones,@A}*/
-   @Promedio * @A
+   @Promedio * @Dias 
+else
+    0
+end
+
+
+
+
+--------------------------------------
+
+
+declare @A real;
+declare @dias real;
+declare @CalcInvVac real;
+declare @Promedio real;
+declare @MesesCalcular int;
+declare @Asignacion1 int;
+declare @MiVar real = 11;
+declare @var1 int;
+declare @var2 int;
+declare @var4 int;
+declare @var3 real;
+
+declare @param_1 nvarchar(60);
+declare @param_2 nvarchar(60);
+declare @param_3 int;
+declare @param_4 int;
+
+
+-- #########
+
+
+set @A = {CantidadTransaccion};
+set @Asignacion1 = 11;
+set @CalcInvVac = {Monto_Anio_VacHistoricas,G_VACACIONES,Acumulado};
+set @Dias = {Tabla_AguinaldoVacaciones,V,hoy,s};
+set @Dias = {Funcion,DiasVacaciones,@A}; 
+set @MesesCalcular =  {Funcion,CalcMesCompletoFechaIngreso,@Asignacion1};
+/*set @Promedio = {Monto_Acumulado_Mes,SC,PMeses,G_VACACIONES,12};*/
+
+set @Promedio = {Monto_Acumulado_Mes,MC,G_VACACIONES,12,Acumulado,D};
+
+set @Promedio = @Promedio / 8;
+     
+set @param_1 = 'AGUINALDO';
+set @param_2 = 'G_VACACIONES';
+set @param_3 = 12;
+set @param_4 = 1;
+set @Promedio = {Funcion,Monto_Acumulado_Cofal,[@param_1,@param_2,@param_3,@param_4]};  
+
+/*
+set @var1 = 10;
+set @var2 = 40;
+set @var4 = 50;
+set @var3 = {Funcion,test_funcion,[@var1,@var2,@var4]}
+*/
+
+
+
+---#######
+
+
+if ({funcion,DiasLiquidacion} > 0) then
+    /*{Total_Acumulado_Anio,MC,D,G_VACACIONES_L,1,pmeses} / 30 * {Funcion,DiasVacaciones,@A}*/
+   @Promedio / 30 * @Dias
+
+
 else
     0
 end
--- Funciones de Formulador/Monto_Acumulado_Cofal.sql
+++ Funciones de Formulador/Monto_Acumulado_Cofal.sql
@@ -2,14 +2,19 @@
 PARAM_1: 'AGUINALDO'|'BONO14'|'OTROS'
 PARAM_2: GRUPO DE ELEMENTOS
 PARAM_3: CANTIDAD DE MESES 
+PARAM_4: 1->MESES COMPLETO COFIÑO, 0->TODOS LOS MESES
+PARAM_5: Devuelve: 0->Promedio, 1->Acumulado, 2->Dias, 3->Meses
 */
 DECLARE @TIPO_PRESTACION NVARCHAR(10) = @PARAM_1
 DECLARE @ElementGroupId NVARCHAR(60) = @PARAM_2
 DECLARE @CANTIDAD INT = CONVERT(INT,@PARAM_3)
 DECLARE @PROMEDIO_COFIÑO INT = CONVERT(INT,@PARAM_4)
+DECLARE @TIPO_RETORNO INT = @PARAM_5
 
 DECLARE @FechaInicial DATE
 DECLARE @FechaFinal DATE
+DECLARE @FechaInicialMes DATE
+DECLARE @FechaFinalMes DATE
 DECLARE @FechaAguinaldo DATE = NULL
 DECLARE @FechaBono DATE = NULL
 
@@ -17,36 +22,50 @@ DECLARE @ElementId NVARCHAR(60)
 DECLARE @ElementType INT
 DECLARE @SUMAINGRESOS REAL = 0
 DECLARE @ACUMULADO REAL = 0
+DECLARE @ACUMULADODIAS INT = 0
 DECLARE @DIAS INT
+DECLARE @DIASMES INT = 0
+DECLARE @CONTAMES INT = 0
+DECLARE @TransId NVARCHAR(20)
 
 DECLARE @EsPrimerDia BIT
+DECLARE @VAR1 INT
+
+IF @TIPO_RETORNO = NULL
+BEGIN
+    SET @TIPO_RETORNO = 0
+END
 
 IF @TIPO_PRESTACION = 'AGUINALDO' OR @TIPO_PRESTACION = 'BONO14'
+BEGIN
     SELECT @FechaAguinaldo = t1.LASTAGUINALDO,
         @FechaBono = t1.LASTBONO14
     FROM PAYROLLEMPL t1
     WHERE t1.EMPLID = '%12'
 
     IF @TIPO_PRESTACION = 'AGUINALDO'
-        IF @FechaAguinaldo = NULL
+        IF @FechaAguinaldo = NULL OR @FechaAguinaldo = CONVERT(DATE,'01/01/1900',103)
             SET @FechaInicial = CONVERT(DATE,'%19',103)
         ELSE
             SET @FechaInicial = @FechaAguinaldo
     IF @TIPO_PRESTACION = 'BONO14'
-        IF @FechaBono = NULL
+        IF @FechaBono = NULL OR @FechaBono = CONVERT(DATE,'01/01/1900',103)
             SET @FechaInicial = CONVERT(DATE,'%19',103)
         ELSE
             SET @FechaInicial = @FechaBono
+END
 ELSE
+BEGIN
     SET @FechaInicial = DATEADD(MONTH,(@CANTIDAD*-1),CONVERT(DATE,'%8',103))
 
     IF @FechaInicial < CONVERT(DATE,'%19',103)
         SET @FechaInicial = CONVERT(DATE,'%19',103)
-
+END
 
 SET @FechaFinal = DATEADD(DAY,-1,CONVERT(DATE,'%8',103))
 
 IF @PROMEDIO_COFIÑO = 1
+BEGIN
     -- Verificar si la fecha inicial es el primer día del mes
     SET @EsPrimerDia = CASE 
         WHEN DAY(@FechaInicial) = 1 THEN 1 
@@ -54,16 +73,21 @@ IF @PROMEDIO_COFIÑO = 1
     END
 
     IF @EsPrimerDia = 0
+    BEGIN
         SET @FechaInicial = DATEADD(DAY,1,EOMONTH(@FechaInicial))
-
+    END
+END
 
 DECLARE PayRollTransEmplElement_Cursor CURSOR FOR
-    select SUM(ROUND(t1.amount,2)) ingresos, SUM(t1.quantity) cantidad
+    select 
+        SUM(ROUND(t1.amount,2)) ingresos, 
+        SUM(t1.quantity) cantidad,
+        t2.FROMDATE, t2.TODATE, t2.TRANSID
     from PAYROLLTRANSEMPLELEMENT t1
     join PAYROLLTRANS t2 on t1.TRANSID = t2.TRANSID
     where t1.ELEMENTID IN (SELECT ElementId
-    FROM PayRollElementPerGroup
-    WHERE ElementGroupId = @ElementGroupId)
+        FROM PayRollElementPerGroup
+        WHERE ElementGroupId = @ElementGroupId)
     AND t1.ELEMENTTYPE = 0
     and t1.EMPLID = '%12'
     AND ((t2.FROMDATE <= @FechaInicial
@@ -73,81 +97,67 @@ DECLARE PayRollTransEmplElement_Cursor CURSOR FOR
         OR (t2.FROMDATE <= @FechaFinal
         AND t2.TODATE >= @FechaFinal))
     and t2.STATUS >= 6
-    group by t2.FROMDATE, t2.TODATE
+    group by t2.FROMDATE, t2.TODATE, t2.TRANSID
 
 OPEN PayRollTransEmplElement_Cursor
 
-FETCH NEXT FROM PayRollTransEmplElement_Cursor INTO @SUMAINGRESOS, @DIAS
+FETCH NEXT FROM PayRollTransEmplElement_Cursor INTO @SUMAINGRESOS, @DIAS, @FechaInicialMes, @FechaFinalMes, @TransId
 
 WHILE @@FETCH_STATUS = 0
 BEGIN
-    SET @ACUMULADO = @ACUMULADO + ROUND(@SUMAINGRESOS,2)
 
-    FETCH NEXT FROM PayRollTransEmplElement_Cursor INTO @SUMAINGRESOS, @DIAS
+    SET @DIASMES = DATEDIFF(DAY,@FechaInicial,@FechaFinal) + 1
+
+    SET @DIAS = (SELECT SUM(t1.QUANTITY)
+    FROM PAYROLLTRANSEMPLELEMENT t1
+    WHERE t1.TRANSID = @TransId
+    AND t1.EMPLID = '%12'
+    AND t1.ELEMENTID IN ('SUSPENSIÓN','SUELDO ORDINARIO','SUELDO ORDINARIO JUB','VACACIONES')
+    AND t1.ELEMENTTYPE = 0)
+
+    IF @DIASMES <> @DIAS
+    BEGIN
+        SET @VAR1= (SELECT COUNT(*) 
+        FROM PAYROLLABSENCETRANS T1
+        INNER JOIN PAYROLLABSENCETABLE T2 ON T1.PAYROLLABSENCETABLEID = T2.PAYROLLABSENCETABLEID 
+        WHERE T1.TRANSDATE BETWEEN @FechaInicialMes AND @FechaFinalMes
+        AND T2.EMPLID = '%12'
+        AND ( T1.PAYROLLABSENCECODEID in ('SUSP. IGSS','SUSP. MATERN') )) 
+
+        IF (@VAR1 + @DIAS) <> (@DIASMES)
+        BEGIN
+            SET @ACUMULADO = @ACUMULADO + ROUND(@SUMAINGRESOS,2)
+            SET @CONTAMES = @CONTAMES + 1
+            SET @ACUMULADODIAS = @ACUMULADODIAS + @DIAS
+        END
+    END
+    ELSE
+    BEGIN
+        SET @ACUMULADO = @ACUMULADO + ROUND(@SUMAINGRESOS,2)
+        SET @CONTAMES = @CONTAMES + 1
+        SET @ACUMULADODIAS = @ACUMULADODIAS + @DIAS
+    END
+
+    FETCH NEXT FROM PayRollTransEmplElement_Cursor INTO @SUMAINGRESOS, @DIAS, @FechaInicialMes, @FechaFinalMes, @TransId
 END
 
 CLOSE PayRollTransEmplElement_Cursor
 DEALLOCATE PayRollTransEmplElement_Cursor
 
-SELECT @ACUMULADO
 
-/*DECLARE ElementGroup_Cursor CURSOR FOR
-SELECT ElementId, ElementType
-FROM PayRollElementPerGroup
-WHERE ElementGroupId = @ElementGroupId
-
-OPEN ElementGroup_Cursor
-
-FETCH NEXT FROM ElementGroup_Cursor INTO @ElementId, @ElementType
-
-WHILE @@FETCH_STATUS = 0
+IF @TIPO_RETORNO = 0
 BEGIN
-
-
-    FETCH NEXT FROM ElementGroup_Cursor INTO @ElementId, @ElementType
+    SELECT ROUND(@ACUMULADO / @CONTAMES,2)
 END
-
-CLOSE ElementGroup_Cursor
-DEALLOCATE ElementGroup_Cursor*/
-
-
-
-
-
-
-DECLARE @ElementGroupId NVARCHAR(60) = 'G_VACACIONES'
-DECLARE @ElementId NVARCHAR(60)
-DECLARE @ElementType INT
-DECLARE @SUMAINGRESOS REAL = 0
-DECLARE @ACUMULADO REAL = 0
-DECLARE @DIAS INT
-
-DECLARE PayRollTransEmplElement_Cursor CURSOR FOR
-    select SUM(t1.amount) ingresos, SUM(t1.quantity) cantidad
-    from PAYROLLTRANSEMPLELEMENT t1
-    join PAYROLLTRANS t2 on t1.TRANSID = t2.TRANSID
-    where t1.ELEMENTID IN (SELECT ElementId
-    FROM PayRollElementPerGroup
-    WHERE ElementGroupId = @ElementGroupId)
-    AND t1.ELEMENTTYPE = 0
-    and t1.EMPLID = '%12'
-    AND t2.FROMDATE >= CONVERT(DATE,'%19',103)
-    AND t2.TODATE <= CONVERT(DATE,'%8',103)
-    and t2.STATUS >= 6
-    group by t2.FROMDATE, t2.TODATE
-
-OPEN PayRollTransEmplElement_Cursor
-
-FETCH NEXT FROM PayRollTransEmplElement_Cursor INTO @SUMAINGRESOS, @DIAS
-
-WHILE @@FETCH_STATUS = 0
+IF @TIPO_RETORNO = 1
 BEGIN
-    SET @ACUMULADO = @ACUMULADO + @SUMAINGRESOS
-
-    FETCH NEXT FROM PayRollTransEmplElement_Cursor INTO @SUMAINGRESOS, @DIAS
+    SELECT @ACUMULADO
 END
-
-CLOSE PayRollTransEmplElement_Cursor
-DEALLOCATE PayRollTransEmplElement_Cursor
-
-SELECT @ACUMULADO
+IF @TIPO_RETORNO = 2
+BEGIN
+    SELECT @ACUMULADODIAS
+END
+IF @TIPO_RETORNO = 3
+BEGIN
+    SELECT @CONTAMES
+END
