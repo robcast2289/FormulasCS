8cd9407970d3b3333e041da14d9a55331f78ba54
Roberto Castro <tonocastrom@gmail.com>
2025-07-10 23:28:26 -0600

    Se modifica formual de Vacaciones liquidacion, y bono 14 anual (problema con el promedio menor al minimo para casos con mas de 1 mes)

--- Formulas CS/VACACIONES L.sql
+++ Formulas CS/VACACIONES L.sql
@@ -4,6 +4,7 @@ declare @Dias real;
 declare @Promedio real;
 declare @A real;
 declare @MinimoLey real;
+declare @SalarioActual real;
 
 declare @param_1 nvarchar(60);
 declare @param_2 nvarchar(60);
@@ -22,6 +23,8 @@ set @param_5 = 0;
 set @Promedio = {Funcion,Monto_Acumulado_Cofal,[@param_1,@param_2,@param_3,@param_4,@param_5]};  
 
 set @MinimoLey = {Salario_Minimo_Zona_Empl};
+set @SalarioActual = {SalarioMensual};
+set @Promedio = si_tern(@Promedio < @MinimoLey, @SalarioActual, @Promedio);
 set @Promedio = si_tern(@Promedio < @MinimoLey, @MinimoLey, @Promedio);
 
 /*set @Promedio = {Monto_Acumulado_Mes,SC,PMeses,G_VACACIONES,11};*/
